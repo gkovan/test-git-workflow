e5c6baae60e6a5df19c943708278a958e1e1af63
gkovan <gkovan@hotmail.com>
2018-09-19 23:07:55 -0400

    change endpont

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -27,7 +27,7 @@ public class GreetingController {
         return "Hello US2";
     }
 
-    @RequestMapping("/hello-us3-gk")
+    @RequestMapping("/hello-us3-gk-test")
     public String greetingHelloUS1() {
         return "Hello US3";
     }
